128092db3ce815fa8df0cdf5f92bf1d31af7cbec
yermhan <dvdmlgn@outlook.com>
2018-02-15 16:23:29 +0000

    vehicle sub-classes

---
+++ CarApplication/CarApplication/Van.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    public enum wheelbase
+    {
+        Short,
+        Medium,
+        Long,
+        Unlisted
+    }
+
+    public enum vanType
+    {
+        CombiVan,
+        Dropside,
+        PanelVan,
+        Pickup,
+        Tipper,
+        Unlisted
+    }
+
+    class Van
+    {
+        public wheelbase WheelBase { get; set; }
+        public vanType Type { get; set; }
+
+    }
+}
